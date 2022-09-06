bf76283eb30b0d5363a4321dec5597a7463b930d
edy <edudiaz7979@gmail.com>
2022-09-06 13:51:06 -0500

    Edy

--- unit_conversions.py
+++ unit_conversions.py
@@ -1,4 +1,3 @@
-
 # By submitting this assignment, I agree to the following:
 # “Aggies do not lie, cheat, or steal, or tolerate those who do”
 # “I have not given or received any unauthorized aid on this assignment”
@@ -8,25 +7,33 @@
 # Eddy
 # Reed
 # Section: 568
-# Assignment: 2-8
-# Date: 30 August 2022
+# Assignment: 3-15 Unit Conversions Lab
+# Date: 6 September 2022
 
-#The following converts Pounds (force) to Newton
 x=float(input("Please enter the quantity to be converted:" ))
+
+#A
 pound_to_newtons=x*4.44822
 print(f"{x:.2f}pounds force is equivalent to {pound_to_newtons:.2f}Newtons")
 
-#The following converts meters to feet
+#B
 meters_to_feet = (float(x * 3.28084))
-final_meters_to_feet = (f'{x} meters is equivalent to {meters_to_feet:.2f} feet')
+final_meters_to_feet = (f'{x:.2f} meters is equivalent to {meters_to_feet:.2f} feet')
 print(final_meters_to_feet)
 
-#The following converts Atmospheres to kilopascals (kPa)
+#C
 atmosphere_to_kilopascal= x*101.325
 print(f'{x:.2f} atmospheres is equivalent to {atmosphere_to_kilopascal:.2f} kilopascals')
 
-#The following converts Watts to BTU per hour
-# 1 watt is 3.41 BTU/h
+#D
 btu_hour = x * 3.41
-print(f'The BTU/h is {btu_hour:.2f}')
+print(f'{x:.2f} The BTU/h is {btu_hour:.2f}')
 
+#E
+liters_to_gallons = (float(x * 15.850323141489))
+final_liters_to_gallons = (f'{x:.2f} liters per second is equivalent to {liters_to_gallons:.2f} US gallons per minute')
+print(final_liters_to_gallons)
+
+#F
+degrees_fahrenheit = float((x * 1.8) + 32)
+print(f'{x:.2f} degrees is equivalent to {degrees_fahrenheit:.2f} degrees')
