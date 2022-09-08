5f8a105cae8d3d7c8460298d7a57fb03d011f193
chris <christophersnell22@gmail.com>
2022-09-08 13:02:33 -0500

    zzz

--- still_more_linear_interpolation.py
+++ still_more_linear_interpolation.py
@@ -9,19 +9,25 @@
 # Section: 568
 # Assignment: 3-16 Still More Linear Interpolation
 # Date: 8 September 2022
-time1=input("Enter time 1:")
-pos_x1=input("Enter the x position of the object at time 1:")
-pos_y1=input("Enter the y position of the object at time 1:")
-pos_z1=input("Enter the z position of the object at time 1:")
+from math import sqrt
 
-time2=input("Enter time 2:")
-pos_x2=input("Enter the x position of the object at time 2:")
-pos_y2=input("Enter the y position of the object at time 2:")
-pos_z2=input("Enter the z position of the object at time 2:")
+time1 = input("Enter time 1:")
+pos_x1 = input("Enter the x position of the object at time 1:")
+pos_y1 = input("Enter the y position of the object at time 1:")
+pos_z1 = input("Enter the z position of the object at time 1:")
 
+time2 = input("Enter time 2:")
+pos_x2 = input("Enter the x position of the object at time 2:")
+pos_y2 = input("Enter the y position of the object at time 2:")
+pos_z2 = input("Enter the z position of the object at time 2:")
 
-CHANGE_IN_TIME=0
-CHANGE_IN_X=0
-CHANGE_IN_Y=0
-CHANGE_IN_Z=0
+time_interval = (time2 - time1) / 4
+time_distance_1 = time1 + time_interval
+time_distance_2 = time_distance_1 + time_interval
+time_distance_3 = time_distance_2 + time_interval
+
+CHANGE_IN_TIME = 0
+CHANGE_IN_X = 0
+CHANGE_IN_Y = 0
+CHANGE_IN_Z = 0
 
