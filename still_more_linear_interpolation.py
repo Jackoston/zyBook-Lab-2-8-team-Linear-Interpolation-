4ef0ccd86a8017bdb5db2282a01d756418b5d84a
chris <christophersnell22@gmail.com>
2022-09-08 12:38:44 -0500

    header

---
+++ still_more_linear_interpolation.py
@@ -0,0 +1,12 @@
+# By submitting this assignment, I agree to the following:
+# “Aggies do not lie, cheat, or steal, or tolerate those who do”
+# “I have not given or received any unauthorized aid on this assignment”
+#
+# Names: Christopher
+# Austin
+# Eddy
+# Reed
+# Section: 568
+# Assignment: 3-16
+# Date: 8 September 2022
+
