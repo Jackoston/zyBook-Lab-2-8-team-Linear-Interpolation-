1887c1f2e476d0cbaf3fc4ca48ea22d0f6097b27
Austin <112511983+aingalls03@users.noreply.github.com>
2022-09-08 13:26:30 -0500

    Update still_more_linear_interpolation.py

--- still_more_linear_interpolation.py
+++ still_more_linear_interpolation.py
@@ -2,48 +2,62 @@
 # “Aggies do not lie, cheat, or steal, or tolerate those who do”
 # “I have not given or received any unauthorized aid on this assignment”
 #
-# Names: Christopher
-# Austin
+# Names: Austin
+# Christopher
 # Eddy
 # Reed
-# Section: 568
-# Assignment: 3-16 Still More Linear Interpolation
-# Date: 8 September 2022
-from math import sqrt
+# Section: 468
+# Assignment:  Lab 3 - 16 (Team): Still More Linear Interpolation
+# Date: 8 Sep. 2022
 
-time1 = float(input("Enter time 1:"))
-print()
-pos_x1 = float(input("Enter the x position of the object at time 1:"))
-print()
-pos_y1 = float(input("Enter the y position of the object at time 1:"))
-print()
-pos_z1 = float(input("Enter the z position of the object at time 1:"))
-print()
+#1st Variables:
+t1 = int(input('Enter time 1: ')) #x1
+x1 = int(input('Enter the x position of the object at time 1: ')) #y1
+y1 = int(input('Enter the y position of the object at time 1: ')) #y1
+z1 = int(input('Enter the z position of the object at time 1: ')) #y1
 
-time2 = float(input("Enter time 2:"))
-print()
-pos_x2 = float(input("Enter the x position of the object at time 2:"))
-print()
-pos_y2 = float(input("Enter the y position of the object at time 2:"))
-print()
-pos_z2 = float(input("Enter the z position of the object at time 2:"))
-print()
+#2nd Variables:
+t2 = int(input('Enter time 2: ')) #x2
+x2 = int(input('Enter the x position of the object at time 2: ')) #y2
+y2 = int(input('Enter the y position of the object at time 2: ')) #y2
+z2 = int(input('Enter the z position of the object at time 2: ')) #y2
 
-time_interval = (time2 - time1) / 4
-time_distance_1 = time1 + time_interval
-time_distance_2 = time_distance_1 + time_interval
-time_distance_3 = time_distance_2 + time_interval
+#Space:
+print('')
 
-CHANGE_IN_TIME = 0
-CHANGE_IN_X = 0
-CHANGE_IN_Y = 0
-CHANGE_IN_Z = 0
+#Time Calculations:
+t_int = (t2 - t1) / 4
+t_dist1 = t1 + t_int
+t_dist2 = t_dist1 + t_int
+t_dist3 = t_dist2 + t_int
 
-new_y1 = pos_y1 + (time_distance_1 - time1) * ((pos_y2 - pos_y1) / (time2 - time1))
-print(new_y1)
+#Linear Interpolation Calculation:
+#1st
+new_x1 = x1 + (t1 - t1) * ((x2 - x1) / (t2 - t1))
+new_y1 = x1 + (t1 - t1) * ((y2 - y1) / (t2 - t1))
+new_z1 = x1 + (t1 - t1) * ((z2 - z1) / (t2 - t1))
+print(f'At time {t1:.2f} seconds the object is at ({new_x1:.3f}, {new_y1:.3f}, {new_z1:.3f})')
 
-print(f"At time {time1:.2f} seconds the object is at ("")")
-print(f"At time {time_distance_1:.2f} seconds the object is at ("")")
-print(f"At time {time_distance_2:.2f}seconds the object is at ("")")
-print(f"At time {time_distance_3:.2f}seconds the object is at ("")")
-print(f"At time {time2:.2f}seconds the object is at ("")")
+#2nd
+new_x2 = x1 + (t_dist1 - t1) * ((x2 - x1) / (t2 - t1))
+new_y2 = x1 + (t_dist1 - t1) * ((x2 - x1) / (t2 - t1))
+new_z2 = x1 + (t_dist1 - t1) * ((x2 - x1) / (t2 - t1))
+print(f'At time 1.25 seconds the object is at ({new_x2:.3f}, {new_y2:.3f}, {new_z2:.3f})')
+
+#3nd
+new_x2 = x1 + (t_dist2 - t1) * ((x2 - x1) / (t2 - t1))
+new_y2 = x1 + (t_dist2 - t1) * ((x2 - x1) / (t2 - t1))
+new_z2 = x1 + (t_dist2 - t1) * ((x2 - x1) / (t2 - t1))
+print(f'At time 1.50 seconds the object is at ({new_x2:.3f}, {new_y2:.3f}, {new_z2:.3f})')
+
+#4nd
+new_x2 = x1 + (t_dist3 - t1) * ((x2 - x1) / (t2 - t1))
+new_y2 = x1 + (t_dist3 - t1) * ((x2 - x1) / (t2 - t1))
+new_z2 = x1 + (t_dist3 - t1) * ((x2 - x1) / (t2 - t1))
+print(f'At time 1.75 seconds the object is at ({new_x2:.3f}, {new_y2:.3f}, {new_z2:.3f})')
+
+#5th
+new_x4 = x1 + (t2 - t1) * ((x2 - x1) / (t2 - t1))
+new_y4 = x1 + (t2 - t1) * ((y2 - y1) / (t2 - t1))
+new_z4 = x1 + (t2 - t1) * ((z2 - z1) / (t2 - t1))
+print(f'At time {t2:.2f} seconds the object is at ({new_x4:.3f}, {new_y4:.3f}, {new_z4:.3f})')
