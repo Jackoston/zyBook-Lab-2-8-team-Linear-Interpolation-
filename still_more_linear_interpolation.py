3dcead75ea0cd4d99a5843c4f2645aca76a48d87
edy <edudiaz7979@gmail.com>
2022-09-08 13:10:00 -0500

    Theses are final print staments also added ints

--- still_more_linear_interpolation.py
+++ still_more_linear_interpolation.py
@@ -11,12 +11,12 @@
 # Date: 8 September 2022
 from math import sqrt
 
-time1 = input("Enter time 1:")
+time1 = int(input("Enter time 1:"))
 pos_x1 = input("Enter the x position of the object at time 1:")
 pos_y1 = input("Enter the y position of the object at time 1:")
 pos_z1 = input("Enter the z position of the object at time 1:")
 
-time2 = input("Enter time 2:")
+time2 = int(input("Enter time 2:"))
 pos_x2 = input("Enter the x position of the object at time 2:")
 pos_y2 = input("Enter the y position of the object at time 2:")
 pos_z2 = input("Enter the z position of the object at time 2:")
