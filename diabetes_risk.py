7ff89ed923b7579d3b0fe28073282b0d158191e2
edy <edudiaz7979@gmail.com>
2022-09-13 13:29:36 -0500

    inputs

--- diabetes_risk.py
+++ diabetes_risk.py
@@ -8,5 +8,13 @@
 # Eddy
 # Reed
 # Section: 568
-# Assignment: 2-8
+# Assignment: 4-c
 # Date: 30 August 2022
+
+sex_input=input("Enter your sex(M/F):")
+age_input=input("Enter your age (years):")
+BMI_input=input("Enter your BMI:")
+hypertension_input =input("Are you on medication for hypertension (Y/N)?")
+steroids_input =input("Are you on steroids (Y/N)?")
+smoker_input= input("Did you used to smoke (Y/N)?")
+history_input=input("Do you have a family history of diabetes (Y/N)?")
