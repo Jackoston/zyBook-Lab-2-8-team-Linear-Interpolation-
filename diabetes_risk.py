e6f599cc757591e63a8de4e9cfe345b2c5c4df57
chris <christophersnell22@gmail.com>
2022-09-13 13:29:47 -0500

    Chris

--- diabetes_risk.py
+++ diabetes_risk.py
@@ -8,5 +8,38 @@
 # Eddy
 # Reed
 # Section: 568
-# Assignment: 2-8
-# Date: 30 August 2022
+# Assignment: 5-3
+# Date: 13 September 2022
+
+#V ariable inputs
+
+
+
+
+
+
+#asign variable to number for equation
+
+# sex ifs
+if upper.sex_input == 'M':
+    sex = 0
+elif upper.sex_input == 'F':
+    sex = 0.879
+
+#BMI ifs
+if BMI_input < 25
+    BMI = 0
+elif 25 <= BMI_input <27.5:
+    BMI = 0.699
+elif 27.5 <= BMI_input < 30
+    BMI = 1.97
+else:
+    BMI = 2.518
+
+
+#Calculations
+n = 6.322 + sex – (0.063 * age) – BMI – hypertension – steroids – smoker – history
+
+
+
+# risk = 100 / (1+en)
