9d4cabcdb9d4292589630a745c06729c2201141b
chris <christophersnell22@gmail.com>
2022-09-13 13:49:26 -0500

    Chris

--- diabetes_risk.py
+++ diabetes_risk.py
@@ -11,14 +11,14 @@
 # Assignment: 5.3
 # Date: 13 September 2022
 
-sex_input=input("Enter your sex(M/F):")
-age=int(input("Enter your age (years):"))
-BMI_input=input("Enter your BMI:")
-hypertension_input =input("Are you on medication for hypertension (Y/N)?")
-steroids_input =input("Are you on steroids (Y/N)?")
-smoker_input=input("Do you smoke cigarettes (Y/N)?")
-Did_Smoke_input= input("Did you used to smoke (Y/N)?")
-history_input=input("Do you have a family history of diabetes (Y/N)?")
+sex_input = input("Enter your sex(M/F):")
+age = int(input("Enter your age (years):"))
+BMI_input = input("Enter your BMI:")
+hypertension_input = input("Are you on medication for hypertension (Y/N)?")
+steroids_input = input("Are you on steroids (Y/N)?")
+smoker_input = input("Do you smoke cigarettes (Y/N)?")
+Did_Smoke_input = input("Did you used to smoke (Y/N)?")
+history_input = input("Do you have a family history of diabetes (Y/N)?")
 
 # Variable inputs
 
@@ -27,7 +27,7 @@ history_input=input("Do you have a family history of diabetes (Y/N)?")
 
 
 
-#asign variable to number for equation
+# asign variable to number for equation
 
 # sex ifs
 if sex_input.upper() == 'M':
@@ -35,32 +35,31 @@ if sex_input.upper() == 'M':
 elif sex_input.upper() == 'F':
     sex = 0.879
 
-#BMI ifs
+# BMI ifs
 if BMI_input < 25:
     BMI = 0
-elif 25 <= BMI_input <27.5:
+elif 25 <= BMI_input < 27.5:
     BMI = 0.699
 elif 27.5 <= BMI_input < 30:
     BMI = 1.97
 else:
     BMI = 2.518
-    
-#Steroid ifs
+
+# Hypertension meds
+if hypertension_input.upper() == 'Y':
+    hypertension = 1.222
+else:
+    hypertension = 0
+
+# steroid meds
 if steroids_input.upper() == "Y":
     steroids = 2.191
 else:
     steroids = 0
 
-#Smoker ifs
-if Did_Smoke_input.upper() == "N":
-    smoke = 0
-elif smoker_input.upper() == "Y":
-    smoker = 0.855
-else:
-    smoker = 0.218
-#Calculations
-#n = 6.322 + sex – (0.063 * age) – BMI – hypertension – steroids – smoker – history
+# Calculations
+n = 6.322 + sex - (0.063 * age) - BMI - hypertension - steroids - smoker - history
 
 
 
-# risk = 100 / (1+en)
+risk = 100 / (1+en)
