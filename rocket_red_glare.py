3a75355ddce34d1b1817cc09bb447f49304b6c97
edy <edudiaz7979@gmail.com>
2022-09-16 17:14:13 -0500

    finish

--- rocket_red_glare.py
+++ rocket_red_glare.py
@@ -1,3 +1,22 @@
+from math import sqrt
+a = 1/ 7
+print(a,"a")
+b = a * 7
+print(b,"b")
+
+c = 2 * a;
+d = 5 * a;
+e = c + d;
+print(e,"e")
+
+x = sqrt(1 / 3);
+print("x =", x);
+y = x * x * 3;
+print("x*x*3 =", y);
+z = x * 3 * x;
+print("x*3*x =", z);
+
+
 
 first_stop = 0.3
 second_go = 0.4
@@ -12,8 +31,19 @@ elif distance_travel == 0.4:
     print('Start the second stage!')
 else:
     print('No action required at this point.')
-# The program only reacted to the number 4, where is
-# printed out Start the second stage. When 3 was input
-# the value after multiplying was 0.30000000000000004
-# else was activated most of the time
 
+
+print("\n lets try that again 'zrrp' \n")
+
+TOL = 1e-17
+
+print(distance_travel)
+if abs(distance_travel - 0.3) < TOL:
+    print('Stop the first stage!')
+elif abs(distance_travel - 0.4) < TOL:
+    print('Start the second stage!')
+else:
+    print('No action required at this point.')
+# yaya now the program works, but when using e-20, the probam does not work
+# pobably because the diffrence isn't that small
+# The code stops working when e-17
