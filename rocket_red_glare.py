ada6d06046e19622c22697cfe158802957f46c2f
edy <edudiaz7979@gmail.com>
2022-09-16 17:15:58 -0500

    finish

--- rocket_red_glare.py
+++ rocket_red_glare.py
@@ -1,3 +1,15 @@
+# By submitting this assignment, I agree to the following:
+# “Aggies do not lie, cheat, or steal, or tolerate those who do”
+# “I have not given or received any unauthorized aid on this assignment”
+#
+# Names: Christopher
+# Austin
+# Eddy
+# Reed
+# Section: 568
+# Assignment: rockets red glare
+# Date: 15 September 2022
+
 from math import sqrt
 a = 1/ 7
 print(a,"a")
