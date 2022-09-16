4029dd210d3dec19f1c6ca3060b7fe1d4971b2c6
Austin <112511983+aingalls03@users.noreply.github.com>
2022-09-16 16:40:16 -0500

    Create pretty_equation.py

---
+++ pretty_equation.py
@@ -0,0 +1,70 @@
+# By submitting this assignment, I agree to the following:
+# “Aggies do not lie, cheat, or steal, or tolerate those who do”
+# “I have not given or received any unauthorized aid on this assignment”
+#
+# Names: Austin
+# Chrisopher
+# Reed
+# Eddy
+# Section: 468
+# Assignment: Lab 4 - 13: Make Change
+# Date: 14th Sep. 2022
+
+# Inputs:
+coeffA = int(input('Please enter the coefficient A: '))
+coeffB = int(input('Please enter the coefficient B: '))
+coeffC = int(input('Please enter the coefficient C: '))
+
+# If Statements for Postive Integers:
+if coeffA > 1 and coeffB > 1 and coeffC > 1:
+    print(f'The quadratic equation is {coeffA}x^2 + {coeffB}x + {coeffC} = 0')
+  
+elif coeffA == 1 and coeffB == 1 and coeffC == 1:
+  print(f'The quadratic equation is x^2 + x + 1 = 0')
+  
+elif coeffA == 1 and coeffB != 1 and coeffC != 1:
+  print(f'The quadratic equation is x^2 + {coeffB}x + {coeffC} = 0')
+elif coeffA == 1 and coeffB == 1 and coeffC != 1:
+  print(f'The quadratic equation is x^2 + x + {coeffC} = 0')
+elif coeffA == 1 and coeffB != 1 and coeffC == 1:
+  print(f'The quadratic equation is x^2 + {coeffB}x + 1 = 0')
+  
+elif coeffB == 1 and coeffA != 1 and coeffC != 1:
+  print(f'The quadratic equation is {coeffA}x^2 + x + {coeffC} = 0')
+elif coeffB == 1 and coeffA == 1 and coeffC != 1:
+  print(f'The quadratic equation is x^2 + x + {coeffC} = 0')
+elif coeffB == 1 and coeffA != 1 and coeffC == 1:
+  print(f'The quadratic equation is {coeffA}x^2 + x + 1 = 0')
+  
+elif coeffC == 1 and coeffA != 1 and coeffB != 1:
+    print(f'The quadratic equation is {coeffA}x^2 + {coeffB}x + 1 = 0')
+elif coeffC == 1 and coeffA == 1 and coeffB != 1:
+    print(f'The quadratic equation is x^2 + {coeffB}x + 1 = 0')
+elif coeffC == 1 and coeffA != 1 and coeffB == 1:
+    print(f'The quadratic equation is {coeffA}x^2 + x + 1 = 0')
+
+elif coeffA == 0 and coeffB == 0 and coeffC == 0:
+  print(f'The quadratic equation is 0')
+  
+elif coeffA == 0 and coeffB != 0 and coeffC != 0:
+    print(f'The quadratic equation is {coeffB}x + {coeffC} = 0')
+elif coeffB == 0:
+    print(f'The quadratic equation is {coeffA}x^2 + {coeffC} = 0')
+elif coeffC == 0:
+    print(f'The quadratic equation is {coeffA}x^2 + {coeffB} = 0')
+
+
+elif coeffA < 0 and coeffB > 0 and coeffC > 0:
+    print(f'The quadratic equation is -{abs(coeffA)}x^2 + {coeffB}x + {coeffC} = 0')
+elif coeffA > 0 and coeffB < 0 and coeffC > 0:
+    print(f'The quadratic equation is {coeffA}x^2 - {abs(coeffB)}x + {coeffC} = 0')
+elif coeffA > 0 and coeffB > 0 and coeffC < 0:
+    print(f'The quadratic equation is {coeffA}x^2 + {coeffB}x - {abs(coeffC)} = 0')
+elif coeffA < 0 and coeffB > 0 and coeffC < 0:
+    print(f'The quadratic equation is -{abs(coeffA)}x^2 + {coeffB}x - {abs(coeffC)} = 0')
+elif coeffA > 0 and coeffB < 0 and coeffC < 0:
+    print(f'The quadratic equation is {coeffA}x^2 - {abs(coeffB)}x - {abs(coeffC)} = 0')
+elif coeffA < 0 and coeffB < 0 and coeffC > 0:
+    print(f'The quadratic equation is -{abs(coeffA)}x^2 - {abs(coeffB)}x + {coeffC} = 0')
+elif coeffA < 0 and coeffB < 0 and coeffC < 0:
+    print(f'The quadratic equation is -{abs(coeffA)}x^2 - {abs(coeffB)}x - {abs(coeffC)} = 0')
