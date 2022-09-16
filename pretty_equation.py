e19a7fa9bee948e0dad3e1ca59416b81599531ec
chris <christophersnell22@gmail.com>
2022-09-16 17:20:26 -0500

    chris try

--- pretty_equation.py
+++ pretty_equation.py
@@ -15,80 +15,82 @@ coeffA = int(input('Please enter the coefficient A: '))
 coeffB = int(input('Please enter the coefficient B: '))
 coeffC = int(input('Please enter the coefficient C: '))
 
-# If Statements for Postive Integers:
-if coeffA > 1 and coeffB > 1 and coeffC > 1:
-    print(f'The quadratic equation is {coeffA}x^2 + {coeffB}x + {coeffC} = 0')
+print('The quadratic equation is', end=' ')
 
-elif coeffA == 1 and coeffB == 1 and coeffC == 1:
-    print(f'The quadratic equation is x^2 + x + 1 = 0')
+if coeffA == 0:
+    if coeffB == 0:
+        if coeffC == 0:
+            water = 12
+        elif coeffC == 1:
+            print(f'1', end=' ')
+        elif coeffC > 0:
+            print(f'{coeffC}', end=' ')
+        elif coeffC < 0:
+            print(f'- {coeffC * -1}', end=' ')
+    elif coeffB == 1:
+        print(f'x', end=' ')
+    elif coeffB > 0:
+        print(f'{coeffB}x', end=' ')
+    elif coeffB < 0:
+        print(f'- {coeffB * -1}x', end=' ')
+elif coeffA == 1:
+    print('x^2', end=' ')
+    if coeffB == 0:
+        water = 12
+    elif coeffB == 1:
+        print(f'+ x', end=' ')
+    elif coeffB == -1:
+        print('- x', end=' ')
+    elif coeffB > 0:
+        print(f'+ {coeffB}x', end=' ')
+    elif coeffB < 0:
+        print(f'- {coeffB * -1}x', end=' ')
+elif coeffA == -1:
+    print('- x^2', end=' ')
+    if coeffB == 0:
+        water = 12
+    elif coeffB == 1:
+        print(f'+ x', end=' ')
+    elif coeffB == -1:
+        print('- x', end=' ')
+    elif coeffB > 0:
+        print(f'+ {coeffB}x', end=' ')
+    elif coeffB < 0:
+        print(f'- {coeffB * -1}x', end=' ')
+elif coeffA > 0:
+    print(f'{coeffA}x^2', end=' ')
+    if coeffB == 0:
+        water = 12
+    elif coeffB == 1:
+        print(f'+ x', end=' ')
+    elif coeffB == -1:
+        print('- x', end=' ')
+    elif coeffB > 0:
+        print(f'+ {coeffB}x', end=' ')
+    elif coeffB < 0:
+        print(f'- {coeffB * -1}x', end=' ')
+elif coeffA < 0:
+    print(f'- {coeffA * -1}x^2', end=' ')
+    if coeffB == 0:
+        water = 12
+    elif coeffB == 1:
+        print(f'+ x', end=' ')
+    elif coeffB == -1:
+        print('- x', end=' ')
+    elif coeffB > 0:
+        print(f'+ {coeffB}x', end=' ')
+    elif coeffB < 0:
+        print(f'- {coeffB * -1}x', end=' ')
 
-elif coeffA == 1 and coeffB != 1 and coeffC != 1:
-    print(f'The quadratic equation is x^2 + {coeffB}x + {coeffC} = 0')
-elif coeffA == 1 and coeffB == 1 and coeffC != 1:
-    print(f'The quadratic equation is x^2 + x + {coeffC} = 0')
-elif coeffA == 1 and coeffB != 1 and coeffC == 1:
-    print(f'The quadratic equation is x^2 + {coeffB}x + 1 = 0')
 
-elif coeffB == 1 and coeffA != 1 and coeffC != 1:
-    print(f'The quadratic equation is {coeffA}x^2 + x + {coeffC} = 0')
-elif coeffB == 1 and coeffA == 1 and coeffC != 1:
-    print(f'The quadratic equation is x^2 + x + {coeffC} = 0')
-elif coeffB == 1 and coeffA != 1 and coeffC == 1:
-    print(f'The quadratic equation is {coeffA}x^2 + x + 1 = 0')
+if coeffC == 0:
+    water = 12
+elif coeffC == 1:
+    print(f'+ 1', end=' ')
+elif coeffC > 0:
+    print(f'+ {coeffC}', end=' ')
+elif coeffC < 0:
+    print(f'- {coeffC * -1}', end=' ')
 
-elif coeffC == 1 and coeffA != 1 and coeffB != 1:
-    print(f'The quadratic equation is {coeffA}x^2 + {coeffB}x + 1 = 0')
-elif coeffC == 1 and coeffA == 1 and coeffB != 1:
-    print(f'The quadratic equation is x^2 + {coeffB}x + 1 = 0')
-elif coeffC == 1 and coeffA != 1 and coeffB == 1:
-    print(f'The quadratic equation is {coeffA}x^2 + x + 1 = 0')
+print('= 0')
 
-
-elif coeffA == 0 and coeffB == 0 and coeffC == 0:
-    print(f'The quadratic equation is 0')
-
-elif coeffA == 0 and coeffB != 0 and coeffC != 0:
-    print(f'The quadratic equation is {coeffB}x + {coeffC} = 0')
-elif coeffA == 0 and coeffB == 0 and coeffC != 0:
-    print(f'The quadratic equation is {coeffC} = 0')
-elif coeffA == 0 and coeffB != 0 and coeffC == 0:
-    print(f'The quadratic equation is {coeffB}x = 0')
-
-elif coeffB == 0 and coeffA != 0 and coeffC != 0:
-    print(f'The quadratic equation is {coeffA}x^2 + {coeffC} = 0')
-elif coeffB == 0 and coeffA == 0 and coeffC != 0:
-    print(f'The quadratic equation is {coeffC} = 0')
-elif coeffB == 0 and coeffA != 0 and coeffC == 0:
-    print(f'The quadratic equation is {coeffA}x^2 = 0')
-
-elif coeffC == 0 and coeffA != 0 and coeffB != 0:
-    print(f'The quadratic equation is {coeffA}x^2 + {coeffB}x = 0')
-elif coeffC == 0 and coeffA == 0 and coeffB != 0:
-    print(f'The quadratic equation is {coeffB}x = 0')
-elif coeffC == 0 and coeffA != 0 and coeffB == 0:
-    print(f'The quadratic equation is {coeffA}x^2 = 0')
-
-
-elif coeffA < -1 and coeffB < -1 and coeffC < -1:
-    print(f'The quadratic equation is - {abs(coeffA)}x^2 - {abs(coeffB)}x - {abs(coeffC)}')
-
-elif coeffA < -1 and coeffB > 1 and coeffC > 1:
-    print(f'The quadratic equation is - {abs(coeffA)}x^2 + {coeffB}x + {coeffC} = 0')
-elif coeffA < -1 and coeffB < -1 and coeffC > 1:
-    print(f'The quadratic equation is - {abs(coeffA)}x^2 - {abs(coeffB)}x + {coeffC} = 0')
-elif coeffA < -1 and coeffB > 1 and coeffC < -1:
-    print(f'The quadratic equation is - {abs(coeffA)}x^2 + {coeffB}x - {abs(coeffC)} = 0')
-
-elif coeffB < -1 and coeffA > 1 and coeffC > 1:
-    print(f'The quadratic equation is {coeffA}x^2 - {abs(coeffB)}x + {coeffC} = 0')
-elif coeffB < -1 and coeffA < -1 and coeffC > 1:
-    print(f'The quadratic equation is - {abs(coeffA)}x^2 - {abs(coeffB)}x + {coeffC} = 0')
-elif coeffB < -1 and coeffA > 1 and coeffC < -1:
-    print(f'The quadratic equation is {coeffA}x^2 - {abs(coeffB)}x - {abs(coeffC)} = 0')
-
-elif coeffC < -1 and coeffA > 1 and coeffB > 1:
-    print(f'The quadratic equation is {coeffA}x^2 + {coeffB}x - {abs(coeffC)} = 0')
-elif coeffC < -1 and coeffA < -1 and coeffB > 1:
-    print(f'The quadratic equation is - {abs(coeffA)}x^2 + {coeffB}x - {abs(coeffC)} = 0')
-elif coeffC < -1 and coeffA > 1 and coeffB < -1:
-    print(f'The quadratic equation is + {coeffA}x^2 - {abs(coeffB)}x - {abs(coeffC)} = 0')
