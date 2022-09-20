956087920568bde5906f9a914fa51d8b73cae778
edy <edudiaz7979@gmail.com>
2022-09-20 13:43:07 -0500

    finish

--- pyramid_area1.py
+++ pyramid_area1.py
@@ -12,13 +12,13 @@
 
 
 from math import sqrt
-sideL = float(input("Enter the side length in meters: "))
-layers = int(input("Enter the number of layers: "))
+sideL = float(input("Enter the side length in meters: ")) #must be float
+layers = int(input("Enter the number of layers: ")) #must be int as kit is unable to use float for range
 area = 0
 n = layers
-for n in range(1, layers + 1):
+for n in range(1, layers + 1): #side layer area
     area = area + n * 3 * (sideL ** 2)
 
-area_top = (sqrt(3) / 4) * (layers * sideL) ** 2
+area_top = (sqrt(3) / 4) * (layers * sideL) ** 2 #this is the area of the gaint triangle cause form top view
 area_total = area + area_top
 print(f'You need {area_total:.2f}m^2 of gold foil to cover the pyramid')
