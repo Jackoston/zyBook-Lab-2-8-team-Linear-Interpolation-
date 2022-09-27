085670cae098a7afb617a5444bf90cd92f03257f
edy <edudiaz7979@gmail.com>
2022-09-27 13:16:50 -0500

    finish

---
+++ kaprekas_constant.py
@@ -0,0 +1,15 @@
+cars = ["1", "3", "2"]
+
+cars.sort()
+
+asscending="".join(cars)
+
+print(cars)
+
+cars.reverse()
+
+dessending=''.join(cars)
+
+print(cars)
+
+print(int(dessending)-int(asscending))
--- pyramid_area1.py
+++ pyramid_area1.py
@@ -12,18 +12,13 @@
 
 
 from math import sqrt
-sideL = float(input("Enter the side length in meters: \n"))
-layers = int(input("Enter the number of layers: \n"))
-area_side = 0
+sideL = float(input("Enter the side length in meters: "))
+layers = int(input("Enter the number of layers: "))
+area = 0
 n = layers
-# range 1- given layers
 for n in range(1, layers + 1):
-    # layers * 3 squares
-    # where squares = side length ^2
-    area_side = area_side + n * 3 * (sideL ** 2)
+    area = area + n * 3 * (sideL ** 2)
 
-# area top is equivalent to area bottom of pyramid
 area_top = (sqrt(3) / 4) * (layers * sideL) ** 2
-# adding the two areas
-area_total = area_side + area_top
+area_total = area + area_top
 print(f'You need {area_total:.2f}m^2 of gold foil to cover the pyramid')
