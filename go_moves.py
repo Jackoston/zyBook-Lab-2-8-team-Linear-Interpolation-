da0686a0814f1e8b1abd69da7c20d2681cfb6a14
edy <edudiaz7979@gmail.com>
2022-10-04 13:14:39 -0500

    finish

--- go_moves.py
+++ go_moves.py
@@ -4,4 +4,19 @@ rowC=[".",".",".",".",".",".",".",".","."]
 rowD=[".",".",".",".",".",".",".",".","."]
 rowE=[".",".",".",".",".",".",".",".","."]
 rowF=[".",".",".",".",".",".",".",".","."]
-board=[]
+rowG=[".",".",".",".",".",".",".",".","."]
+rowH=[".",".",".",".",".",".",".",".","."]
+rowI=[".",".",".",".",".",".",".",".","."]
+board=[rowA,rowB,rowC,rowD,rowF,rowG,rowF]
+
+for r in range(len(board)):
+    for i in board[r]:
+        print(i,end=" ")
+    print()
+want_Stop=input("stop? (Y/N)")
+turn_number=0
+while want_Stop!="Y":
+    if turn_number%2==0:
+        #whites turn
+    else:
+        #blacks turn
