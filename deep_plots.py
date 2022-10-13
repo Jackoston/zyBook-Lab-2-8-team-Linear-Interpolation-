3ca9bf11a7c7e902681cf40d4e4ca2476c74d232
chris <christophersnell22@gmail.com>
2022-10-13 13:18:07 -0500

    chris try

--- deep_plots.py
+++ deep_plots.py
@@ -15,7 +15,7 @@ import matplotlib.pyplot as plt
 from math import e
 
 # t & y Values:
-t = [0, 0.45, 1.1, 1.75, 2.25, 2.7]
+t5 = [0, 0.45, 1.1, 1.75, 2.25, 2.7]
 y = [0, 0.23, 0.4, 0.18, 0.07, 0.01]
 
 # Line Calculation:
@@ -26,27 +26,30 @@ for t in range(150):
     t_list.append(t)
 print(t_list)
 
+
 # Dot Calculation:
 # y(t) = t^2 exp(-t^2)
 # y(t) = t^4 exp(-t^2)
 fun1_list = []
-for i in t:
-    fun1 = [(i ** 2) * (e ** (-i ** 2))]
+for t in t_list:
+    fun1 = [(t ** 2) * (e ** (-t ** 2))]
     fun1_list.append(fun1)
 print(fun1_list)
 
 fun2_list = []
-for i in t:
-    fun2 = [(i ** 4) * (e ** (-i ** 2))]
+for t in t_list:
+    fun2 = [(t ** 4) * (e ** (-t ** 2))]
     fun2_list.append(fun2)
 print(fun2_list)
 
 # Plots:
 plt.subplot(2, 1, 1)
-plt.plot(t, fun1_list, 'ko')
+plt.plot(t5, y, 'ko')
+plt.plot(t_list, fun1_list)
+plt.plot(t_list, fun2_list)
 
 plt.subplot(2, 1, 2)
-plt.plot(t, fun2_list)
+plt.plot(t_list, fun2_list)
 
 # Plot Print:
 plt.show()
