c5579eee0dd663fd0afe873f4e26cd40bdcb312d
chris <christophersnell22@gmail.com>
2022-11-03 13:05:02 -0500

    chris try

--- cheesy_llamas.py
+++ cheesy_llamas.py
@@ -1,4 +1,15 @@
 # Program that takes arbitrary x, y data pairs, then calculates linear interpolation / extrapolation for any x
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+#
+# Name:         Christopher
+#               Eddy
+#               Reed
+#               Austin
+# Section:      YOUR SECTION NUMBER
+# Assignment:   Lab Cheesy Llamas
+# Date:         3 November 2022
 
 def get_user_data():
     """ Parameters: (none)
