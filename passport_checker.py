6e0b37100d9b33eafad2dea4da5d6c87a5185151
Austin <112511983+aingalls03@users.noreply.github.com>
2022-11-08 13:02:35 -0600

    Create passport_checker.py

---
+++ passport_checker.py
@@ -0,0 +1,11 @@
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+#
+# Name:         Austin
+#               Christopher
+#               Reed
+#               Eddy
+# Section:      468
+# Assignment:   10 - 13 Debugged Code
+# Date:         Nov. 1st, 2022
