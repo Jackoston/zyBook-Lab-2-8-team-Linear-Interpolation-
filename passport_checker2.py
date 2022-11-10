1e79076bc0f3f5ef6e71658ea0776dc7beaa8eec
chris <christophersnell22@gmail.com>
2022-11-10 13:39:18 -0600

    workinprogress commit

--- passport_checker2.py
+++ passport_checker2.py
@@ -10,10 +10,36 @@
 # Assignment:   10 - 13 Debugged Code
 # Date:         Nov. 1st, 2022
 
+allFile = open('scanned_passports.txt', 'r', newline='')
+passports = allFile.read().split('\r\n\r')
 
-print("goo:slimbim"["goo:slimbim".index(":")+1:])
+allFile.close()
 
 eyeColorDict={ "amb":1, "blu":1, "brn":1, "gry":1, "grn":1, "hzl":1, "oth":1}
+value_person = []
+
+hclCount = 0
+for person in passports:
+    temp = person.split()
+    value_person.append(temp)
+
+for g in range(len(value_person)):
+    value_person[g].sort()
+
+for index in value_person:
+
+# tier1, passport ID
+tier1 = []
+for i in value_person:
+    if i[-1][:3] == 'pid' and i[-1][4] == '0' and len(i[-1]) == 13:
+        tier1.append(i)
+for t in range(len(tier1)):
+    print(tier1[t])
+
+
+# tier2, eye color
+print("goo:slimbim"["goo:slimbim".index(":")+1:])
+
 eyr="amb"
 pid="791118269"
 hgt="hgt:183cm"
