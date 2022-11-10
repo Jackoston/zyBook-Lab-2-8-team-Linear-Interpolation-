fe9e22eb5183f6e487989f53c5e2e6c88824f2de
edy <edudiaz7979@gmail.com>
2022-11-10 13:03:01 -0600

    finish

--- passport_checker2.py
+++ passport_checker2.py
@@ -9,3 +9,11 @@
 # Section:      468
 # Assignment:   10 - 13 Debugged Code
 # Date:         Nov. 1st, 2022
+
+
+print("goo:slimbim"["goo:slimbim".index(":")+1:])
+
+eyeColorDict={ "amb":1, "blu":1, "brn":1, "gry":1, "grn":1, "hzl":1, "oth":1}
+
+pid="791118269"
+if len(pid)
