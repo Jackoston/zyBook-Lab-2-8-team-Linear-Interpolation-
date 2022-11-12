6afa649746dddae043d765a8a0402a29367293c5
chris <christophersnell22@gmail.com>
2022-11-12 13:13:47 -0600

    chris checker2

--- passport_checker2.py
+++ passport_checker2.py
@@ -9,15 +9,14 @@
 # Section:      468
 # Assignment:   10 - 13 Debugged Code
 # Date:         Nov. 1st, 2022
-
-allFile = open('scanned_passports.txt', 'r', newline='')
+file = input('Enter the name of the file: ')
+allFile = open(f'{file}', 'r', newline='')
 passports = allFile.read().split('\r\n\r')
 
 allFile.close()
 
-eyeColorDict={ "amb":1, "blu":1, "brn":1, "gry":1, "grn":1, "hzl":1, "oth":1}
 value_person = []
-
+valid_passports = []
 hclCount = 0
 for person in passports:
     temp = person.split()
@@ -26,36 +25,72 @@ for person in passports:
 for g in range(len(value_person)):
     value_person[g].sort()
 
-for index in value_person:
+ecl_list = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
+# loop for every passport and every single thing in the passport
+for thing in value_person:
+    num_valid = 0
+    for single in thing:
+        try:
+            # check1: if birth year is valid
+            if single[:3] == 'byr' and 1920 <= int(single[4:]) <= 2005:
+                num_valid += 1
+        except ValueError:
+            pass
+        try:
+            # check2: if issue year is valid
+            if single[:3] == 'iyr' and 2012 <= int(single[4:]) <= 2022:
+                num_valid += 1
+        except ValueError:
+            pass
+        try:
+            # check3: if expiration year is valid
+            if single[:3] == 'eyr' and 2022 <= int(single[4:]) <= 2032:
+                num_valid += 1
+        except ValueError:
+            pass
+        try:
+            # check4: if height is valid (cm)
+            if single[:3] == 'hgt' and single[-1] == 'm' and 150 <= int(single[4:-2]) <= 193:
+                num_valid += 1
+        except ValueError:
+            pass
+        try:
+            # check4: if height is valid (in)
+            if single[:3] == 'hgt' and single[-1] == 'n' and 59 <= int(single[4:-2]) <= 76:
+                num_valid += 1
+        except ValueError:
+            pass
+        try:
+            # check5: if eye color is valid
+            if single[:3] == 'ecl' and single[4:] in ecl_list:
+                num_valid += 1
+        except ValueError:
+            pass
+        try:
+            # check6: if passport ID is valid
+            if single[:3] == 'pid' and len(single[4:]) == 9 and int(single[4:]):
+                num_valid += 1
+        except ValueError:
+            pass
+        try:
+            # check7: if country ID is valid
+            if single[:3] == 'cid' and 99 < int(single[4:]) < 1000:
+                num_valid += 1
+        except ValueError:
+            pass
+        if num_valid == 7:
+            valid_passports.append(thing)
 
-# tier1, passport ID
-tier1 = []
-for i in value_person:
-    if i[-1][:3] == 'pid' and i[-1][4] == '0' and len(i[-1]) == 13:
-        tier1.append(i)
-for t in range(len(tier1)):
-    print(tier1[t])
 
+print(f'There are {len(valid_passports)} valid passports')
+writing_passports = []
 
-# tier2, eye color
-print("goo:slimbim"["goo:slimbim".index(":")+1:])
+for passport in passports:
+    for thing in valid_passports:
+        if thing[-1] in passport:
+            writing_passports.append(passport)
 
-eyr="amb"
-pid="791118269"
-hgt="hgt:183cm"
-hgtbool=
-eyr="2022"
-if len(pid)==9 and pid.index("0")!=0:
-    try:
-        if eyeColorDict.get(eyr)==1:
-            if hgt[-2:-1]=="in"or hgt[-2:-1]=="cm":
-                try:
-                    hieghtNum=int(hgt[0:hgt.index("cm")])
-                except:
-                    hieghtNum=int(hgt[0:hgt.index("in")])
-                if int(eyr)>=2022 and int(eyr)<=2032:
-                    print("cows go moo")
-    except:
-        print("you stupid :P")
+validFile = open('valid_passports2.txt', 'w', newline='')
+for passport in writing_passports:
+    validFile.write(f'{passport}\n')
 
-#wbigwbibgwigwibgwibogew
