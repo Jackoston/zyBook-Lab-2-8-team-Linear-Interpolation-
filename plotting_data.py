97cf1986e586785353efb2edcd80617adff56308
chris <christophersnell22@gmail.com>
2022-11-15 13:57:48 -0600

    chris start

--- plotting_data.py
+++ plotting_data.py
@@ -1,3 +1,18 @@
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+#
+# Name:         Austin
+#               Christopher
+#               Reed
+#               Eddy
+# Section:      468
+# Assignment:   12 - 17 plotting data
+# Date:         November 15, 2022
+
+import numpy
+import matplotlib.pyplot as plt
+
 readFile = open('WeatherDataCLL.csv', 'r')
 days = []
 temp_max = []   # -2 index
@@ -15,4 +30,33 @@ for line in readFile:
     except ValueError:
         pass
 
-print(days)
+# x for plot 1
+day_num_plt1x = []
+for i in range(len(days)):
+    day_num_plt1x.append(i)
+print(day_num_plt1x)
+
+# y1 for plot 1, max temp
+max_temp_plt1y = []
+for day in days:
+    max_temp_plt1y.append(day[-2])
+print(max_temp_plt1y)
+
+# y2 for plot 1, wind speed average
+avg_windspeed_plt1y = []
+for day in days:
+    avg_windspeed_plt1y.append(day[1])
+print(avg_windspeed_plt1y)
+
+left_axis = plt.subplot(2, 2, 1)
+right_axis = left_axis.twinx()
+plt.legend(loc='lower left')
+left_axis.set_xlabel('date')
+left_axis.plot(day_num_plt1x, max_temp_plt1y, 'r-', label='Max Temp')
+left_axis.set_ylabel('Maximum Temperature, F')
+right_axis.plot(day_num_plt1x, avg_windspeed_plt1y, 'b-', label='Avg Wind')
+right_axis.set_ylabel('Average wind speed, mph')
+plt.title('Maximum Temperature and Average Wind Speed')
+plt.subplot(2, 2, 2)
+plt.tight_layout()
+plt.show()
